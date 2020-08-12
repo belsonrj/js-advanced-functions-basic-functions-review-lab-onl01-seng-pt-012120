c90ad48b97eec53435555579c3e326fc695974d7
Rick Belson <belsonrj@aol.com>
2020-08-12 18:57:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ function saturdayFun(activity= "roller-skate") {
   return(`This Saturday, I want to ${activity}!`)
 }
 
+function mondayWork(work="go to the office") {
+  return(`This Monday, I will ${work}`)
+}
