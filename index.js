3fa21e228130215bc95c003026ad645dfd01086e
Rick Belson <belsonrj@aol.com>
2020-08-12 19:33:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ function wrapAdjective(flair="*") {
 
 const Calculator = {
   add: function(a, b) {
-    
+    return a+b;
+  },
+  subtract: function(a, b) {
+    return a-b;
+  },
+  multiply: function(a, b) {
+    return a*b;
+  },
+  divide: function(a, b) {
+    return a/b;
   }
 }
+
+function actionApplyer(start, fns) {
+  for (let i = 0; i < fns.length; i++) {
+    start = fns[i](start);
+  }
